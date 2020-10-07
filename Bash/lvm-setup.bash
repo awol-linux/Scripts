2f5c848df8e55609259dd9dd8e213b936008e6b4
avremy <awolosh1@gmail.com>
2020-10-07 12:47:34 -0400

    more work lvm-setup

--- Bash/lvm-setup.bash
+++ Bash/lvm-setup.bash
@@ -1,4 +1,6 @@
 #/bin/env bash
+set -x
+
 fn() {
 	while true; do
 		case $input in
@@ -21,20 +23,27 @@ fn() {
 
 
 #Assign pv
-
-echo 'Please enter physcal device path or press Enter to skip'
-read pvinput
-if [[ ! -z $pvinput ]] ; then
-	echo $pvinput
-	for partition in $(sudo fdisk -l | cut --delimiter=' ' --fields=1  | grep dev)
-	do
-		
-	done
-else
-	echo 'Not assigning any physical devices'	
-fi
-
-pvs
+while true ; do
+	echo 'Please enter physcal device path or press Enter to skip'
+	read pvinput
+	if [[ ! -z $pvinput ]] ; then
+		echo $pvinput
+		if [[ $(sudo fdisk -l | awk '/^\/dev/ {print $1}' | grep "$pvinput") ]] ; then
+			echo "partition $pvinput valid"
+			echo would you like to enter anther partition
+			read input
+		else
+			echo "partition $pvinput not a valid partition"
+		fi
+		fn $input
+		if [output == false]
+			break
+		fi
+	else
+		"OK no assigning any physical volumes"
+	fi
+	pvs
+done
 
 # assign volume group
 echo "Enter volume group name"
@@ -46,8 +55,13 @@ if [[ ! -z $pvinput ]] ; then
 			physical-volumes=$output
 		else
 			echo "Please enter disk names"
-			read pvs
-		fi 
+			read pvnames
+			if [[ $(sudo pvs | grep "$pvnames" ) ]] ; then
+				echo $pvnames
+			else 
+				echo "Device $pvnames is not assigned"
+			fi
+		fi
 fi
 #	
 #else 
