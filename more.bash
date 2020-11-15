a8f051873ec0df20bd1253258322e55791b4e7c4
awol-linux <awolosh1@gmail.com>
2020-11-14 19:06:44 -0500

    added another meow script

--- more.bash
+++ more.bash
@@ -1,24 +1,29 @@
+# day 1
+
 declare -A rules
 rules+=([meow]=cisco [awol]=linux)
 for key in ${!rules[@]}; do
     echo more ${rules[${key}]}\: ${key}
 done
 
+
+# day 2
+
 echo "who are you talking to"
-
-
-
-
 while true ;do
-	read input
-	case $input in
-		meow )
-        		echo "more cisco"
-			break;;
-   		 awol )
-        		echo "more linux"
-			break;;
-		*)
-			echo "invalid input"
-	esac
+        read input
+        case $input in
+                meow )
+                        echo "more rhel"
+                        break;;
+                awol )
+                        echo "more rhel"
+                        break;;
+                kelvin )
+                        echo "more cisco"
+                        break ;;
+                *)
+                        echo "invalid input"
+                        ;;
+        esac
 done
